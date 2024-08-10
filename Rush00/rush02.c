044bbceb265ebb718061b28065c0f8d81a32b17e
Tony Besnard <tobesnar@c1r6p7.42lehavre.fr>
2024-08-10 12:11:10 +0200

    draw_first_line

--- Rush00/rush02.c
+++ Rush00/rush02.c
@@ -6,37 +6,36 @@
 /*   By: tobesnar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 11:10:31 by tobesnar          #+#    #+#             */
-/*   Updated: 2024/08/10 11:51:14 by tobesnar         ###   ########.fr       */
+/*   Updated: 2024/08/10 12:11:00 by tobesnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-void	ft_putchar(char *c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	print_vert(int	x)
+void	draw_first_line(int x)
 {
-	ft_putchar("A");
+	ft_putchar('A');
 	if (x >= 3)
 	{
 		x -= 2;
-		while (x != 0)
+		while (x != `0)
 		{
-			ft_putchar("B");
+			ft_putchar('B');
 			x--;
 		}
-		ft_putchar("A");
+		ft_putchar('A');
 	}
 	if (x == 2)
-		ft_putchar("A");
+		ft_putchar('A');
+	ft_putchar('\n');
 }
 
 int	main(void)
 {
-	int a = 44;
-
-	print_vert(a);
+	print_vert(10);
 	return 0;
 }
