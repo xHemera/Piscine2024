ba909255a737a3a35d52d3f40bbffec4fb74344e
Tony Besnard <tobesnar@c1r6p6.42lehavre.fr>
2024-08-15 17:40:20 +0200

    finished C06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tobesnar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 17:24:50 by tobesnar          #+#    #+#             */
+/*   Updated: 2024/08/15 17:39:45 by tobesnar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	*swp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			swp = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = swp;
+			i = 1;
+		}
+		else
+			i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write (1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
