5b04fa2c63db0c2f7c523c5bf89e3de4641e9e48
Tony Besnard <tobesnar@c1r6p6.42lehavre.fr>
2024-08-15 13:40:06 +0200

    C06

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tobesnar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 12:01:35 by tobesnar          #+#    #+#             */
+/*   Updated: 2024/08/15 12:10:48 by tobesnar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc > 0)
+	{
+		i = 1;
+		while (argv[i])
+		{
+			write(1, argv[i], ft_strlen(argv[i]));
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
